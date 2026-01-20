b72e0b48a1f5f3b53079bc4b30dc31e9d4fb6e19
Alessandro Silvestri <alessandro.silvestri.work@gmail.com>
2026-01-20 19:36:21 +0000

    added the genar total component (only static)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 // import { Button } from "flowbite-react";
 import RowInvoice from "./components/RowInvoice";
 import HeaderInvoiceTable from "./HeaderInvoiceTable";
+import TotalInvoice from "./components/TotalInvoice";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -19,6 +18,7 @@ function App() {
         unitCost={120}
       />
       <RowInvoice item={2} description="carrots" quantity={5} unitCost={5} />
+      <TotalInvoice />
     </>
   );
 }
---
+++ src/components/TotalInvoice.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+let TotalInvoice = () => {
+  const [total, setTotal] = useState(0);
+  return (
+    <div>
+      <div className="grid grid-cols-[1fr_2fr_100px_80px_1fr] gap-5 mt-2">
+        <div className="p-4 text-center rounded"></div>
+        <div className="p-4 text-center rounded"></div>
+        <div className="p-4 text-center rounded"></div>
+        <div className="p-4 text-center rounded"></div>
+        <div className="p-4 text-black text-center rounded">TOTAL: £ {total}</div>
+      </div>
+    </div>
+  );
+};
+
+export default TotalInvoice;
